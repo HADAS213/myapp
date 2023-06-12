acb432675d4c7712032c832c35b0174fa2339c7f
HADAS213 <HADAsmar014@gmail.com>
2023-06-12 13:21:13 +0300

    color

--- src/hw/color.js
+++ src/hw/color.js
@@ -1,26 +1,30 @@
 import React, { useState } from 'react'
 
-function Color() {
+export default function Color() {
 
-    const[color,setColor]=useState("silver");
+    // const[color,setColor]=useState("silver");
     const inputRef = useRef;
+    inputRef = React.createRef()
 
-    const onChangeColor = () => {
-        props.setColor(inputRef.current.value);
+    const changeColor = () => {
+        // current-> כדי לדבר עם האלמנט
+        // console.log(this.inputRef.current.value);
+        let input_val = this.inputRef.current.value;
+        this.setState({ color: input_val });
+        // input_val.style={{color:this.state.color}};
     }
 
-    return (
-        <div className='col-md-4 mx-auto d-flex'>
-            <input ref={inputRef} defaultValue={"2024-01-01"} type='date' className='form-control' />
-            <button onClick={onChangeColor}>change</button>
-
-        </div>
-    )
-  return (
-    <div>
-
-    </div>
-  )
+   
+        return (
+            <div>
+                <h2>Enter your favorite color: {this.state.color}</h2>
+                <input onInput={this.changeColor} ref={this.inputRef} type="text" />
+                
+            </div>
+        );
+    
+      
+ 
 }
 
-export default Color
+
