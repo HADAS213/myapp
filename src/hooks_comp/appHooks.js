aef6b52365f5ab30c5bb9fbe622fd43b1870e94e
HADAS213 <HADAsmar014@gmail.com>
2023-06-12 10:03:42 +0300

    npm

---
+++ src/hooks_comp/appHooks.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from './counter'
+
+export default function AppHooks(){
+    return(
+        <React.Fragment>
+            <hr/>
+            <hr/>
+            <hr/>
+            <Counter/>
+        </React.Fragment>
+    )
+}
